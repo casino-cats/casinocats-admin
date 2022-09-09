844a261de0d822213ef056d2910315de6dc0735d
kise999 <kise@pku.edu.cn>
2022-09-09 19:59:12 +0800

    login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,34 +1,7 @@
-import { useWallet } from "@solana/wallet-adapter-react";
 import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
-import React, { useContext, useEffect } from "react";
 import LogoPng from "../images/logo.png";
-import { auth, getMe, getNonce } from "../utils/lib/mutations";
 
 const Login = () => {
-  const { publicKey, signMessage } = useWallet();
-
-  // useEffect(() => {
-  //   const signin = async () => {
-  //     if (publicKey) {
-  //       const result = await getNonce({ publicKey: publicKey.toBase58() });
-  //       console.log(result);
-  //       // const encodedMessage = new TextEncoder().encode(
-  //       //   "Welcome to casinocats"
-  //       // );
-  //       // if (!signMessage) return;
-  //       // const signedMessage = await signMessage(encodedMessage);
-  //       // const result: { access_token: string; user: string } = await auth({
-  //       //   publicKey: publicKey.toBytes(),
-  //       //   signature: Buffer.from(signedMessage),
-  //       // });
-  //       // const accessToken = result.access_token;
-  //       const me = await getMe();
-  //     }
-
-  //     signin();
-  //   };
-  // }, []);
-
   return (
     <div className="w-screen h-screen bg-gray-800">
       <div className="flex items-center justify-center h-full">
